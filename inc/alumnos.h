11212e8f8dc7542a1fa9efe5551441de6e5a671a
CJMaffrand <cjmaffrand@invap.com.ar>
2021-07-12 22:05:26 -0300

    Se agrega documentación, para probar Doxygen, se modifica el makefile para generar documentación.

--- inc/alumnos.h
+++ inc/alumnos.h
@@ -29,10 +29,11 @@ extern "C" {
 
 /*=====[Definitions of public data types]====================================*/
 
+//! Estructura de Alumnos
 typedef struct alumno_s {
-    char apellidos[30];
-    char nombres[30];
-    char documento[11];
+    char apellidos[30];     //!< Apellido del alumno
+    char nombres[30];       //!< Nombres del alumno
+    char documento[11];     //!< DNI del alumno
 } const * alumno_t;
 
 /*=====[Definitions of public global variables]==============================*/
